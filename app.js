d8ff30d87190068879918e9abf85d7ea4e2c6413
MominIshtiaq <momin989ishtiaq@gmail.com>
2025-02-12 17:17:02 +0500

    added express middleware and create global error middleware

--- app.js
+++ app.js
@@ -4,12 +4,28 @@ import { PORT, NODE_ENV } from "./config/env.js";
 import authRouter from "./routes/auth.routes.js";
 import userRouter from "./routes/user.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
+import errorMiddleware from "./middlewares/error.middleware.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
+
+// express built-in middlewares
+
+// This allows our application to hanlde JSON data sent in Requests or API calls
+app.use(express.json());
+
+// This helps us to process the form data sent via HTML forms in a simple format
+app.use(express.urlencoded({ extended: false }));
+
+// This  helps to read cookies from the incoming requests so our application can store user data
+app.use(cookieParser);
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/subscriptions", subscriptionRouter);
+
+// Custom Middleware
+app.use(errorMiddleware);
 
 app.get("/", (req, res) => {
   res.send("Welcome to the Subscription tracker API!");
